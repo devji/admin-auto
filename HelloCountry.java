23df08fbe9653519df2901ba60884bab6144c0d1
devji <debasish.bj@gmail.com>
2014-10-12 17:36:33 -0400

    modified country, added state

--- HelloCountry.java
+++ HelloCountry.java
@@ -3,9 +3,6 @@ public class HelloCountry {
    
    public static void main(String [] args){
       System.out.println("Hello Country");
-<<<<<<< HEAD
-      System.out.println("Hello Country Again");
-=======
->>>>>>> 6688a5c41d7cf2b2163ae4c900af8cec58085946
+      System.out.println("Hello USA");
    }
 }
---
+++ HelloState.java
@@ -0,0 +1,7 @@
+
+public class HelloState {
+   
+   public static void main(String [] args){
+      System.out.println("Hello State");
+   }
+}

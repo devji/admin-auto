f0fcdc1b82e217103fbeb55c68e429717c662853
devji <debasish.bj@gmail.com>
2014-10-25 01:04:38 -0400

    calc change1:q

---
+++ Calc.java
@@ -0,0 +1,6 @@
+public class Calc  {
+     public static void main(String [] args){
+          System.out.println("Calculator prog");
+          System.out.println("Calculator prog again");
+     }
+}

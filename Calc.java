9ce4900be7e0fc6073fe42d15231f09de07093a1
devji <debasish.bj@gmail.com>
2014-10-25 01:08:51 -0400

    Revert "calc change1:q"
    This reverts commit f0fcdc1b82e217103fbeb55c68e429717c662853.

--- Calc.java
+++
@@ -1,6 +0,0 @@
-public class Calc  {
-     public static void main(String [] args){
-          System.out.println("Calculator prog");
-          System.out.println("Calculator prog again");
-     }
-}
